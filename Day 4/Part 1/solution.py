b647c49af0169e3d638d45a81a5f41a91e2cf2f6
3met <47290143+3met@users.noreply.github.com>
2020-12-04 19:06:05 -0500

    Upload day 4 solutions

--- Day 4/Part 1/solution.py
+++ Day 4/Part 1/solution.py
@@ -7,18 +7,38 @@ Section 1
 """
 
 import os
+import re
 
 SELF_DIR = os.path.dirname(__file__)
 INPUT_PATH = os.path.join(SELF_DIR, 'puzzle_input.txt')
 
 # Fetch puzzle input
 with open(INPUT_PATH, 'r') as file:
-	PUZZLE_INPUT = file.read().split()
+	PUZZLE_INPUT = file.read().split('\n\n')
 
 
 def main(puzzle_input):
 	# Main Function
-	pass
+	expressions = [
+		'byr',
+		'iyr',
+		'eyr',
+		'hgt',
+		'hcl',
+		'ecl',
+		'pid',
+	]
+
+	count = 0
+
+	for passport in puzzle_input:
+		for e in expressions:
+			if not re.search(e, passport):
+				break
+		else:
+			count += 1
+
+	print(count)
 
 
 if __name__ == '__main__':
--- Day 4/Part 2/solution.py
+++ Day 4/Part 2/solution.py
@@ -7,18 +7,39 @@ Section 2
 """
 
 import os
+import re
+from datetime import datetime
 
 SELF_DIR = os.path.dirname(__file__)
 INPUT_PATH = os.path.join(SELF_DIR, 'puzzle_input.txt')
 
 # Fetch puzzle input
 with open(INPUT_PATH, 'r') as file:
-	PUZZLE_INPUT = file.read().split()
+	PUZZLE_INPUT = file.read().split('\n\n')
 
 
 def main(puzzle_input):
 	# Main Function
-	pass
+	expressions = [
+		'byr:(19[2-9][0-9]|200[0-2])',
+		'iyr:(201[0-9]|2020)',
+		'eyr:(202[0-9]|2030)',
+		'hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)',
+		'hcl:#[0-9a-f]{6}',
+		'ecl:(amb|blu|brn|gry|grn|hzl|oth)',
+		'pid:[0-9]{9}([^0-9]|$)',
+	]
+
+	count = 0
+
+	for passport in puzzle_input:
+		for e in expressions:
+			if not re.search(e, passport):
+				break
+		else:
+			count += 1
+
+	print(count)
 
 
 if __name__ == '__main__':
