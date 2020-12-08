b8a423bf3168c0f49b9a20b6bdf35cf3568ed19f
3met <47290143+3met@users.noreply.github.com>
2020-12-08 18:45:25 -0500

    Upload day 7 solutions

--- Day 7/Part 1/solution.py
+++ Day 7/Part 1/solution.py
@@ -13,12 +13,73 @@ INPUT_PATH = os.path.join(SELF_DIR, 'puzzle_input.txt')
 
 # Fetch puzzle input
 with open(INPUT_PATH, 'r') as file:
-	PUZZLE_INPUT = file.read().split()
+	PUZZLE_INPUT = file.read().split('\n')
+
+class Bag:
+	bag_index = {}
+
+	def __init__(self, name, children):
+		Bag.bag_index[name] = self
+		self.children = set(children)
+
+		# Denote whether the bag can contain a shiny gold bag
+		# directly or through is children; 'None' indicates the 
+		# value is not known
+		if 'shiny gold' in children:
+			self.has_gold = True
+		else:
+			self.has_gold = None
+
+	def set_child(self, child):
+		children.add(child)
+
+	def check_gold(self):
+		if self.has_gold:
+			return True
+		elif self.has_gold == False:
+			return False
+		else:
+			for bag in self.children:
+				if Bag.bag_index[bag].check_gold():
+					#self.has_gold = True
+					return True
+			#self.has_gold = False
+			return False
 
 
 def main(puzzle_input):
 	# Main Function
-	pass
+
+	# --- Parse Input ---
+	for line in puzzle_input:
+		words = line.split(' ')
+		# Note that names are always two words in length
+		name = ' '.join(words[0:2])
+		children = []
+
+		if words[4] != 'no':	# Account for when there are no bags
+			children.append(' '.join(words[5:7]))
+
+			# Loop through remaining children bags
+			# Note that distancing between bag names is constant
+			i = 0
+			while True:
+				i += 1
+				if bool(words[5+i*4:7+i*4]):
+					children.append(' '.join(words[5+i*4:7+i*4]))
+				else:
+					break
+
+		Bag(name, children)
+
+	# --- Calculate Valid Bags ---
+	total = 0
+
+	for bag in Bag.bag_index.values():
+		if bag.check_gold():
+			total += 1
+
+	print(total)
 
 
 if __name__ == '__main__':
--- Day 7/Part 2/solution.py
+++ Day 7/Part 2/solution.py
@@ -1,7 +1,7 @@
 
 """
 Advent of Code 2020
-By Emet Behredt
+By Emet Behrendt
 Day 7
 Section 2
 """
@@ -13,12 +13,68 @@ INPUT_PATH = os.path.join(SELF_DIR, 'puzzle_input.txt')
 
 # Fetch puzzle input
 with open(INPUT_PATH, 'r') as file:
-	PUZZLE_INPUT = file.read().split()
+	PUZZLE_INPUT = file.read().split('\n')
+
+
+class Bag:
+	bag_index = {}
+
+	def __init__(self, name, children):
+		Bag.bag_index[name] = self
+		self.children = dict(children)
+
+		# Number of children (including grandchildren, etc) 
+		# that the bag contains
+		if len(children) == 0:
+			self.n_children = 0
+		else:
+			self.n_children = None
+
+	def calc_n_children(self):
+		if self.n_children is not None:
+			return self.n_children
+		else:
+			total = 0
+			for c in self.children:				
+				# Number of specific child bag
+				total += self.children[c]
+				# Number of bag A * number of sub-bag that bag A has
+				total += self.children[c] * Bag.bag_index[c].calc_n_children()
+				self.n_children = total
+			return total
 
 
 def main(puzzle_input):
 	# Main Function
-	pass
+
+	# --- Parse Input ---
+	for line in puzzle_input:
+		words = line.split(' ')
+		# Note that names are always two words in length
+		name = ' '.join(words[0:2])
+		children = {}
+
+		if words[4] != 'no':	# Account for when there are no bags
+			# Pair child with number of that child
+			children[' '.join(words[5:7])] = int(words[4])
+
+			# Loop through remaining children bags
+			# Note that distancing between bag names is constant
+			i = 0
+			while True:
+				i += 1
+				if bool(words[5+i*4:7+i*4]):
+					children[' '.join(words[5:7])] = int(words[4])
+					children[' '.join(words[5+i*4:7+i*4])] = int(words[4+i*4])
+				else:
+					break
+
+		Bag(name, children)
+
+	# --- Calculate Valid Bags ---
+	total = Bag.bag_index['shiny gold'].calc_n_children()
+
+	print(total)
 
 
 if __name__ == '__main__':
