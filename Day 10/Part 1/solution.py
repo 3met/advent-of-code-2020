891eb414020e8cbeea03f85fd7c39b0888153699
3met <47290143+3met@users.noreply.github.com>
2020-12-10 02:40:03 -0500

    Upload day 10 answers

--- Day 10/Part 1/solution.py
+++ Day 10/Part 1/solution.py
@@ -1,7 +1,7 @@
 
 """
 Advent of Code 2020
-By Emet Behredt
+By Emet Behrendt
 Day 10
 Section 1
 """
@@ -18,8 +18,25 @@ with open(INPUT_PATH, 'r') as file:
 
 def main(puzzle_input):
 	# Main Function
-	pass
+	adapters = [int(x) for x in puzzle_input]
 
+	adapters.append(0)	# outlet
+	adapters.append(max(adapters) + 3)	# device
+
+	adapters.sort()
+
+	n_one = 0
+	n_three = 0
+
+	for i in range(len(adapters) - 1):
+		dif = adapters[i+1] - adapters[i]
+
+		if dif == 1:
+			n_one += 1
+		elif dif == 3:
+			n_three += 1
+
+	print(n_one * n_three)
 
 if __name__ == '__main__':
 	main(PUZZLE_INPUT)
--- Day 10/Part 2/solution.py
+++ Day 10/Part 2/solution.py
@@ -1,7 +1,7 @@
 
 """
 Advent of Code 2020
-By Emet Behredt
+By Emet Behrendt
 Day 10
 Section 2
 """
@@ -16,9 +16,41 @@ with open(INPUT_PATH, 'r') as file:
 	PUZZLE_INPUT = file.read().split()
 
 
+class Adapter:
+	'''
+		Contains data about the adaptor
+	'''
+
+	def __init__(self, joltage, n_paths=None):
+		self.joltage = joltage
+		# Number of paths (NoneType for unknown)
+		self.n_paths = n_paths
+
+
 def main(puzzle_input):
 	# Main Function
-	pass
+	adapters = [Adapter(int(x)) for x in puzzle_input]
+
+	# Calculating n paths to outlet
+	adapters.append(Adapter(0, n_paths=1))	# outlet
+	adapters.sort(key=lambda a: a.joltage)
+	adapters.append(Adapter(adapters[-1].joltage + 3))	# device
+
+	max_dif = 3	# Max difference
+
+	for i in range(1, len(adapters)):
+		adapters[i].n_paths = 0
+
+		# Find the sum of n_paths between all the possible connections
+		for j in range(1, max_dif+1):	# Limits the distance to 3 apart
+			dif = abs(adapters[i].joltage - adapters[i-j].joltage)
+			if dif > max_dif:
+				break
+			else:
+				adapters[i].n_paths += adapters[i-j].n_paths
+
+	# Total number of paths from phone to outlet
+	print(adapters[-1].n_paths)
 
 
 if __name__ == '__main__':
